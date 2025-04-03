e0075ed6647501b1508510329679549ff3a71d35
Basar Bashir <134760873+Basar-Bashir@users.noreply.github.com>
2025-04-03 17:15:12 +0530

    Update incertion.c

--- assign 1/incertion.c
+++ assign 1/incertion.c
@@ -1,86 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-
-struct Node {
-    int data;
-    struct Node* next;
-};
-
-
-struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
-}
-
-
-void insertAtBeginning(struct Node** headRef, int data) {
-    struct Node* newNode = createNode(data);
-    newNode->next = *headRef;
-    *headRef = newNode;
-}
-
-
-void insertAtEnd(struct Node** headRef, int data) {
-    struct Node* newNode = createNode(data);
-    if (*headRef == NULL) {
-        *headRef = newNode;
-    } else {
-        struct Node* temp = *headRef;
-        while (temp->next != NULL) {
-            temp = temp->next;
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
         }
-        temp->next = newNode;
+        arr[j + 1] = key;
     }
 }
 
-
-void insertAtPosition(struct Node** headRef, int data, int position) {
-    struct Node* newNode = createNode(data);
-    if (position == 0) {
-        newNode->next = *headRef;
-        *headRef = newNode;
-        return;
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
-    struct Node* temp = *headRef;
-    for (int i = 0; i < position - 1 && temp != NULL; i++) {
-        temp = temp->next;
-    }
-    if (temp == NULL) {
-        printf("Position out of bounds\n");
-        free(newNode);
-    } else {
-        newNode->next = temp->next;
-        temp->next = newNode;
-    }
+    printf("\n");
 }
-
-
-void printList(struct Node* head) {
-    struct Node* temp = head;
-    while (temp != NULL) {
-        printf("%d -> ", temp->data);
-        temp = temp->next;
-    }
-    printf("NULL\n");
-}
-
 
 int main() {
-    struct Node* head = NULL;
-
-    
-    insertAtBeginning(&head, 1);
-    insertAtEnd(&head, 3);
-    insertAtPosition(&head, 2, 1);
-    insertAtEnd(&head, 4);
-    insertAtPosition(&head, 0, 0);
-
-    
-    printf("Linked list after insertions: ");
-    printList(head);
-
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr, n);
+    printArray(arr, n);
     return 0;
 }
