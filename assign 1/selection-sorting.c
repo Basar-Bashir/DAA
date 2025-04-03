c5c9210fac7e8b1570807f82af2e38855c38eeae
Basar Bashir <134760873+Basar-Bashir@users.noreply.github.com>
2025-04-03 17:15:45 +0530

    Update selection-sorting.c

--- assign 1/selection-sorting.c
+++ assign 1/selection-sorting.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
 
-
 void selectionSort(int arr[], int n) {
-    int i, j, minIndex, temp;
-
-    for (i = 0; i < n - 1; i++) {
-        
-        minIndex = i;
-
-        
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j; 
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
             }
         }
-
-        
-        temp = arr[minIndex];
-        arr[minIndex] = arr[i];
+        int temp = arr[minIdx];
+        arr[minIdx] = arr[i];
         arr[i] = temp;
     }
 }
@@ -29,18 +21,10 @@ void printArray(int arr[], int n) {
     printf("\n");
 }
 
-
 int main() {
-    int arr[] = {6, 5, 4, 3, 2};
+    int arr[] = {64, 25, 12, 22, 11};
     int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: \n");
-    printArray(arr, n);
-
     selectionSort(arr, n);
-
-    printf("Sorted array: \n");
     printArray(arr, n);
-
     return 0;
 }
